dc48ceebbddda20fd000f329ba7667a31cdd6f3c
apple <apple@apples-MacBook-Air.local>
2025-02-17 18:18:08 +0900

    feat: user entity 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UserModule } from './user/user.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite', //database type
+      database: 'chartjs.sqlite', // database 파일명
+      entities: [], //엔티티 리스트
+      synchronize: true,//데이터베이스에 스키마를 동기화
+      logging: true,//sql 실행 로그 확인
+    }),
+    UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
