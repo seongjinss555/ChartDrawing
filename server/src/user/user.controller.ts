28e0c0f4d154acfe2b6cf3f9e50893667c2e6eaa
apple <apple@apples-MacBook-Air.local>
2025-02-17 20:41:35 +0900

    설명: 문제 해결 및 설정 수정

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Body, Get, Post, Param, Put, Delete } from '@nestjs/common';
+import { UserService } from './user.service';
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService){};
+
+    @Put("/update/:email")
+    updateUser(@Param('email')email: string, @Body()user: User){
+        return this.userService.updateUser(email, user);
+    }
+}
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
-@Injectable()
-export class UserService {}
+@Injectable() //의존성 주입
+export class UserService {
+    constructor(
+        @InjectRepository(User) private userRepository: Repository<User>,
+    ){} // 레포 주입
+    
+    createUser(user): Promise<User>{
+        return this.userRepository.save(user);
+    }
+
+    async getUser(email: string){
+        const result = await this.userRepository.findOne({
+            where: {email},
+        });
+        return result;
+    } // 한 명의 유저 찾기
+
+    async updateUser(email, _user){
+        const user: User = await this.getUser(email);
+        console.log(_user);
+        user.username = _user.username;
+        user.password = _user.password;
+        console.log(user);
+        this.userRepository.save(user);
+    } // 유저 정보 업데이트, username, password만 변경
+
+    deleteUser(email: any){
+        return this.userRepository.delete({email});
+    }
+}
